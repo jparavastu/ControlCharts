6fd0a898d386faa8186e409b00be34428fb10cc7
jparavastu <kishorepj@gmail.com>
2017-04-07 10:59:00 -0700

    updated read me

--- Data_preprocessing.scala
+++ Data_preprocessing.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.sql.SQLContext
 val sqlContext = new SQLContext(sc)
 
-val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("/user/hive/sample/testdata.csv")
+val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("../testdata.csv")
 
 //pass these values through UI
 val colum_agg = "Year"
@@ -10,4 +10,4 @@ val colum = "Weight"
 val df1 = df.groupBy(colum_agg).agg(max(colum).cast("double").alias("MAX"),min(colum).cast("double").alias("MIN"),round(avg(colum),2).cast("double").alias("MEAN"),count(colum_agg).alias("sample"),round(stddev(colum),2).alias("STDDEV"))
 val data = df1.withColumn("RANGE",round(($"MAX" - $"MIN"),2))
 
-data.repartition(1).write.format("com.databricks.spark.csv").mode("overwrite").option("header", "true").save("/user/hive/sample/process_data")
+data.repartition(1).write.format("com.databricks.spark.csv").mode("overwrite").option("header", "true").save("../process_data")
